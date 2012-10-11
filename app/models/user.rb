3a1822eb8ef3f8590a6aeaa26278b8b7b10ddab4
royaditi <aroy91@gmail.com>
2012-10-11 16:00:46 -0500

    Minor changes to User Form and Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ActiveRecord::Base
-  #attr_accessible :password, :confirmation
-  #attr_accessor :password, :confirmation
-  #validates :password, :presence =>true,
-                    #:length => { :minimum => 5, :maximum => 40 },
-                    #:confirmation =>true
-  #validates_confirmation_of :password
+  validates :email, :presence => true
 end

9e0090786a70104b41821d14b18f338bc99bc3b3
royaditi <aroy91@gmail.com>
2012-11-16 16:34:19 -0600

    Changed tests. password reset tests pending

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,6 @@ class PagesController < ApplicationController
 
   def home
   	@title = "Home"
-  end
-
-  
+  end  
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,11 +9,4 @@ describe PagesController do
     end
   end
 
-  describe "GET 'register'" do
-    it "should be successful" do
-      get 'register'
-      response.should be_success
-    end
-  end
-
 end

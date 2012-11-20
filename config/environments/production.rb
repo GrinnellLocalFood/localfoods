a057d0eb699f1f7fa7535a98258aef3110235c42
royaditi <aroy91@gmail.com>
2012-11-19 20:40:21 -0600

    ActionMailer production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,4 +52,15 @@ Localfoods::Application.configure do
 
   # default path for action mailer
   config.action_mailer.default_url_options = {:host => "localfood.herokuapp.com"}
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'], 
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'localfood.heroku.com',
+  :authentication => :plain,
+}
+
+ActionMailer::Base.delivery_method = :smtp
 end

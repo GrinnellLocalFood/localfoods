1da735d4eac23e30be25fbbd0de8fd0843de4421
royaditi <aroy91@gmail.com>
2012-11-19 21:31:56 -0600

    Changed migration for heroku

--- db/migrate/20121114042132_add_farm_properties.rb
+++ db/migrate/20121114042132_add_farm_properties.rb
@@ -1,9 +1,11 @@
 class AddFarmProperties < ActiveRecord::Migration
   def self.up
-  	add_column :farms, :description, :url
+  	add_column :farms,:description
+        add_column, :farms, :url
   end
 
   def self.down
-  	remove_column :farms, :description
+  	remove_column :farms, :url
+        remove_column :farms, :description
   end
 end

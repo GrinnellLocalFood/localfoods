0b94adc8c7851953b75da1f9ca0fbef78a9450c9
royaditi <aroy91@gmail.com>
2012-11-26 18:22:43 -0600

    Added inventory edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126224336) do
+ActiveRecord::Schema.define(:version => 20121126235610) do
 
   create_table "inventories", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "url"
     t.string   "description"
+    t.string   "display_name"
   end
 
   create_table "items", :force => true do |t|

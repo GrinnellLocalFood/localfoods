4e676daab8d1dd3e13494538ba3b893d30e94369
gulatiak <gulatiak@grinnell.edu>
2012-12-01 14:06:50 -0600

    changed gitignore again

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@ class Inventory < ActiveRecord::Base
 	belongs_to :user, :foreign_key => "id" #a user's "id" column is the foreign key# :autosave => false 
 	has_many :item, :foreign_key => "inventory_id", :autosave => true
 
-	has_attached_file :photo, :url => "/public/system/photos/:id/:style/:basename.:extension", :styles => {
+	has_attached_file :photo, :url => "/system/photos/:id/:style/:basename.:extension", :styles => {
 		:large => "700x700>",
 		:medium => "300x300>",
 		:small => "150x150>"

0b0bd330e6aedec7dcdb5b9acd463691815b0569
Aaltan Ahmad <ahmadaal@grinnell.edu>
2012-12-06 22:09:38 -0600

    started tests for ItemsController

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -47,21 +47,39 @@ describe ItemsController do
     end
   end
 
-  describe "For signed-in users" do
-    
-    describe "admin" do
-      before(:each) do
-        @admin = test_sign_in(Factory(:admin))
-        @producer = Factory(:producer)
-        @item = Factory(:item)
-      end
+#   describe "For signed-in users" do
+#     before(:each) do
+#       @attr = { :user_id => -1, 
+#         :first_name => "Example", 
+#         :last_name => "User", 
+#         :email => "user@example.com", 
+#         :password => "password",
+#         :password_confirmation => "password"}
 
-      describe "should be able to add an item" do
-        # post :create, { :item => @item, :inventory_id => @producer.inventory.id }
-        # @producer.inventory.item.first.description.should eq(@item.description)
-      end
-    end
-  end
+#       # @admin_attr = @attr.merge(:admin => true)
+#       # @producer_attr = @attr.merge(:producer => true)
+#       # @coord_attr = @attr.merge(:coordinator => true)
+#       # @admin = User.create!(@user_attr)
+#       # @producer = User.create!({ :first_name => "b", :last_name => "a", :email => "asdf@asdf.com", :password => "asdfasdf", :password_confirmation => "asdfasdf", :producer => true})
+#       # @coordinator = User.create!(@coord_attr)
+#       # sign_in @admin
+#       end
+
+#     describe "admin" do
+      
+#       before(:each) do
+#       end
+
+#       describe "should be able to add an item" do
+
+#         @admin = Factory(:user, :admin => true, :email => "blah@blah.com")
+#         @producer = Factory(:user, :producer => true)
+#         post :create, :item => @item #, :inventory_id => @producer.inventory.id }
+#         @producer.inventory.item.first.description.should eq(@item.description)
+
+#       end
+#     end
+#   end
 
 
-end
+# end

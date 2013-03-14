3b05d883fa8e9d746455deba2eb065546c91120f
royaditi <aroy91@gmail.com>
2013-03-13 21:57:42 -0500

    allowing only valid values for cart, not showing add to cart on unavailable items

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,9 +4,13 @@ class CartItemsController < ApplicationController
 		@cart_item = CartItem.where("cart_id = ? AND item_id = ?", current_user.cart, @item).first
 		if @cart_item.nil?
 			@cart_item = CartItem.create!(:cart_id => current_user.cart.id, :quantity => @item.minorder, :item_id => @item.id)
+			if @item.minorder > 1
+			flash[:notice] = "Added #(@item.minorder} #{@item.name.pluralize} to cart"
+			else
 			flash[:notice] = "Added #{@item.name} to cart"
+		end
 		else
-			@cart_item.quantity = @cart_item.quantity + 1.0
+			@cart_item.quantity = @cart_item.quantity + 1
 			@cart_item.save
 			flash[:notice] = "Updated #{@item.name} in cart"
 		end
@@ -19,9 +23,13 @@ class CartItemsController < ApplicationController
 
 	end
 
+	def edit
+		@cart_item = CartItem.find(params[:id])
+	end
+
 	def destroy
-		@item = CartItem.find(params[:id])
-		@item.destroy
+		@cart_item = CartItem.find(params[:id])
+		@cart_item.destroy
 		respond_to do |format|
 			format.js { render show_in_modal_cart_path(current_user) }
         	format.html { redirect_to(cart_path(current_user), :notice => 'Item successfully deleted.')}
@@ -32,9 +40,8 @@ class CartItemsController < ApplicationController
 
 	def update
 		@cart_item = CartItem.find(params[:cart_item_id])
-
 		respond_to do |format|
-			if @cart_item.update_attributes(params[:cart_item])
+			if params[:cart_item][:quantity] == "0" || @cart_item.update_attributes(params[:cart_item])
 				if params[:cart_item][:quantity] == "0"
 					@cart_item.destroy
 					@notice = 'Item successfully deleted'
@@ -46,8 +53,11 @@ class CartItemsController < ApplicationController
 				format.html { redirect_to(cart_path(params[:id], :item => @cart_item.id, :disable => true), :notice => @notice) }
 				format.xml  { head :ok }       
 			else
-				format.js { render show_in_modal_cart_path(params[:id]) }
-				format.html { redirect_to(cart_path(params[:id]), :error => 'Item was not successfully updated.') }
+				errors = @cart_item.item.name + " could not be updated. "
+				if(@cart_item.errors.any?)
+					errors += @cart_item.errors.full_messages.to_sentence + "."
+				end
+				format.js { render :js => "var error = '#{errors}'; alert(error);" }
 				format.xml  { render :xml => @cart_item.errors, :status => :unprocessable_entity }
 			end
 		end

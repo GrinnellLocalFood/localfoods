aed638d884ae6f6bba226a2faa2e83da64627db6
Colin Brooks <brooksco@grinnell.edu>
2013-03-17 16:15:57 -0500

    added things

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require bootstrap
+//= require_tree
+//= require jquery-tablesorter/jquery.metadata
+//= require jquery-tablesorter/jquery.tablesorter
+//= require jquery-tablesorter/jquery.tablesorter.widgets
+//= require jquery-tablesorter/addons/pager/jquery.tablesorter.pager

2068c3a8ab5fb7e9ff302a01e631d674559f63b4
royaditi <aroy91@gmail.com>
2013-03-29 15:41:24 -0500

    Revert "bad pull"
    This reverts commit e8de63c60c211cc3d2a3d2141fc3a2f0df283319, reversing
    changes made to b806f7f1cb36bde2d634ed1696ced4da09dcb1e0.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ Localfoods::Application.config.session_store :cookie_store, :key => '_localfoods
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
+# (create the session table with "rake db:sessions:create")
 # Localfoods::Application.config.session_store :active_record_store

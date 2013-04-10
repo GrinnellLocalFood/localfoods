f2e02e42dd46a1d229bceb2a6d397b944b8000d7
Colin Brooks <brooksco@grinnell.edu>
2013-04-09 20:59:10 -0500

    fixed a bunch of asset location issues and calls, big forms are now positioned much better

--- public/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-//= require jquery
-//= require bootstrap
-//= require_tree
-//= require jquery-tablesorter/jquery.metadata
-//= require jquery-tablesorter/jquery.tablesorter
-//= require jquery-tablesorter/jquery.tablesorter.widgets
-//= require jquery-tablesorter/addons/pager/jquery.tablesorter.pager

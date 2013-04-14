dd0b8f49d31bd94894e5cc17152b6c44d34c3779
Akshay Gulati <gulatiak@grinnell.edu>
2013-04-14 18:08:34 -0500

    paypal encrypted

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,31 +1,32 @@
 class PaymentNotification < ActiveRecord::Base
-  attr_accessible :cart_id, :params, :status, :transaction
+  attr_accessible :cart_id, :params, :status, :transaction_id
 
   belongs_to :cart
   serialize :params
   after_create :create_purchases
 
 
-	def create_purchases
+  def create_purchases
 
-		if(status == "Completed")
-			current_cart = Cart.find(cart_id)
-			current_cart.cart_items.each do |cart_item|
-				@old_purchase = Purchase.where("user_id =? AND item_id = ?", current_cart.user.id, cart_item.item.id).first
-				if @old_purchase.nil? || @old_purchase.paid == false
-					@purchase = Purchase.new(:item_id => cart_item.item_id, 
-							 :user_id => current_cart.user.id, 
-							 :quantity => cart_item.quantity,
-							 :unit_price => cart_item.item.price,
-							 :paid => true)
-					@purchase.save
-				else
-					@old_purchase.quantity += cart_item.quantity
-					@old_purchase.save
-				end
-			end
-			current_cart.clear_all_items
-		end
-	end
+  	if status == "Completed" && params[:secret] == APP_CONFIG[:paypal_secret] && params[:receiver_email] == APP_CONFIG[:paypal_email]
+
+  		current_cart = Cart.find(cart_id)
+  		current_cart.cart_items.each do |cart_item|
+  			@old_purchase = Purchase.where("user_id =? AND item_id = ?", current_cart.user.id, cart_item.item.id).first
+  			if @old_purchase.nil? || @old_purchase.paid == false
+  				@purchase = Purchase.new(:item_id => cart_item.item_id, 
+  					:user_id => current_cart.user.id, 
+  					:quantity => cart_item.quantity,
+  					:unit_price => cart_item.item.price,
+  					:paid => true)
+  				@purchase.save
+  			else
+  				@old_purchase.quantity += cart_item.quantity
+  				@old_purchase.save
+  			end
+  		end
+  		current_cart.clear_all_items
+  	end
+  end
 
 end

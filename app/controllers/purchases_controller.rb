1cb708b90dc173df9a51d25e6f0d645a84f73abf
Akshay Gulati <gulatiak@grinnell.edu>
2013-04-18 15:56:34 -0500

    All order are sorted now

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+	helper_method :sort_column, :sort_direction
 	
 	def new
 		@purchase = Purchase.new
@@ -22,9 +23,57 @@ class PurchasesController < ApplicationController
 	def all_orders
 		@title = "All Orders"
 		@purchases = Purchase.all
+		@purchases = sorted_purchases
 	end
 
 	def process_order
 		redirect_to :action => 'index'
 	end
+
+	def sorted_purchases
+
+		if sort_column == "buyer"
+			if sort_direction == "asc"
+				return @purchases.sort_by{|i| i.user.name }
+			else
+				return @purchases.sort_by{|i| i.user.name }.reverse
+			end
+		end
+
+		if sort_column == "producer"
+			if sort_direction == "asc"
+				return @purchases.sort_by{|i| i.item.inventory.display_name }
+			else
+				return @purchases.sort_by{|i| i.item.inventory.display_name }.reverse
+			end
+		end
+
+		if sort_column == "product"
+			if sort_direction == "asc"
+				return @purchases.sort_by{|i| i.item.name }
+			else
+				return @purchases.sort_by{|i| i.item.name }.reverse
+			end
+		end
+
+		if sort_column == "paid_for"
+			if sort_direction == "asc"
+				return @purchases.sort_by{|i| i.paid.to_s }
+			else
+				return @purchases.sort_by{|i| i.paid.to_s }.reverse
+			end
+		end
+		return @purchases
+	end
+
+
+    
+  def sort_column
+    ["buyer", "producer", "product", "paid_for"].include?(params[:sort]) ? params[:sort] : "product"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end

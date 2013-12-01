e6375492076aec4fb7349558e06d34f1d4a2cb3d
Marta Kostova <mkostova@camplight.net>
2013-12-01 20:53:12 +0200

    setuped default locale to be bg setuped currency to be from the default locale converted all tabs to spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,24 @@
+
 class ApplicationController < ActionController::Base
-	protect_from_forgery
-	include SessionsHelper
+  protect_from_forgery
+  include SessionsHelper
 
-	before_filter :require_login 
+  before_filter :require_login 
 
-	def current_cart
-		if current_user
-			current_cart = Cart.find_by_user_id(current_user)
-		end
-	end
+  def number_to_currency(number, options = {})
+    options[:locale] ||= I18n.locale
+    super(number, options)
+  end
+  def current_cart
+    if current_user
+      current_cart = Cart.find_by_user_id(current_user)
+    end
+  end
 
-	private
+  private
 
-	def require_login	
-		redirect_to login_path unless current_user
-	end
+  def require_login 
+    redirect_to login_path unless current_user
+  end
 
 end
